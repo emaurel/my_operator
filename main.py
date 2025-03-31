90952ad63d7480610e03e3338d2619f9a1e1e697
Edgar Maurel <edgar.maurel@epitech.eu>
2025-03-31 11:25:28 +0200

    feat - change operator name

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 from tercen.client import context as ctx
 import numpy as np
 
-tercenCtx = ctx.TercenContext(
-    workflowId="03efcb35db925880b87a09a004013815",
-    stepId="64f5120a-9c2f-41ea-8cd4-5eec8bccb792",
-    username="admin", # if using the local Tercen instance
-    password="admin", # if using the local Tercen instance
-    serviceUri = "http://tercen:5400/" # if using the local Tercen instance
-)
+tercenCtx = ctx.TercenContext()
 
 df = (
     tercenCtx
