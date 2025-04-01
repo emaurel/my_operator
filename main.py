62933d91893ca7b02c68aa6b9250a23becb0d483
Edgar Maurel <edgar.maurel@epitech.eu>
2025-04-01 12:19:26 +0200

    fix : centroids as ints

--- main.py
+++ main.py
@@ -114,7 +114,7 @@ df = groups.reshape(-1, 3)
 
 dataset = pd.DataFrame(df, columns=['centroid', '.ci', '.ri'])
 
-dataset = dataset.astype({'.ci': 'int32', '.ri': 'int32'})
+dataset = dataset.astype({'centroid' : 'int32', '.ci': 'int32', '.ri': 'int32'})
 
 
 print("done")
